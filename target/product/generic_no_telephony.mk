f097fbbc15a1b9f72270f7b114dffeda9bf7779c
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2014-06-23 12:54:45 +0300

    Remove Music.apk, use Launcher3

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -33,8 +33,7 @@ PRODUCT_PACKAGES := \
     InputDevices \
     Keyguard \
     LatinIME \
-    Launcher2 \
-    Music \
+    Launcher3 \
     MusicFX \
     OneTimeInitializer \
     PrintSpooler \
